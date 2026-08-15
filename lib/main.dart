4243c4000b80ced2c4a8fac9a805cedf0452fccf
noshadramdani <noshadprofessional69@gmail.com>
2026-08-15 12:43:01 +0500

    feat: initialize notification services

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:applylog/core/notifications/prsentations/providers/notification_provider.dart';
+import 'package:applylog/core/notifications/prsentations/providers/schedule_follow_up_provider.dart';
 import 'package:applylog/core/router/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -6,15 +8,19 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(ProviderScope(child: const MyApp()));
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final container = ProviderContainer();
+  await container.read(notificationRepositoryProvider).initialize();
+  runApp(UncontrolledProviderScope(container: container, child: const MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
+
     return MaterialApp.router(
       routerConfig: router,
       debugShowCheckedModeBanner: false,
