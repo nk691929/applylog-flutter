46bc822c377a9ac36aa4b472880a6905e91282a6
noshadramdani <noshadprofessional69@gmail.com>
2026-08-17 07:37:58 +0500

    feat: wire StatefulShellRoute bottom nav and theme provider

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -4,6 +4,9 @@ import 'package:applylog/features/applications/presentation/screens/application_
 import 'package:applylog/features/applications/presentation/screens/application_list_screen.dart';
 import 'package:applylog/features/auth/presentation/providers/auth_provider.dart';
 import 'package:applylog/features/auth/presentation/screens/auth_screen.dart';
+import 'package:applylog/features/dashboard/presentation/screens/dashboard_screen.dart';
+import 'package:applylog/features/settings/presentation/screens/settings_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -23,10 +26,6 @@ final routerProvider = Provider<GoRouter>((ref) {
     routes: [
       GoRoute(path: '/auth', builder: (context, state) => const AuthScreen()),
       GoRoute(
-        path: '/',
-        builder: (context, state) => const ApplicationListScreen(),
-      ),
-      GoRoute(
         path: '/add',
         builder: (context, state) => const AddApplicationScreen(),
       ),
@@ -36,6 +35,57 @@ final routerProvider = Provider<GoRouter>((ref) {
           final app = state.extra as Application;
           return ApplicationDetailScreen(application: app);
         },
+      ),
+      StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) {
+          return Scaffold(
+            body: navigationShell,
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: navigationShell.currentIndex,
+              onTap: (value) => navigationShell.goBranch(value),
+              items: const [
+                BottomNavigationBarItem(
+                  label: "Dashboard",
+                  icon: Icon(Icons.dashboard),
+                ),
+                BottomNavigationBarItem(
+                  label: "Applications",
+                  icon: Icon(Icons.settings_applications),
+                ),
+                BottomNavigationBarItem(
+                  label: "Settings",
+                  icon: Icon(Icons.settings),
+                ),
+              ],
+            ),
+          );
+        },
+        branches: [
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: "/",
+                builder: (context, state) => const DashboardScreen(),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: "/applications",
+                builder: (context, state) => const ApplicationListScreen(),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: "/settings",
+                builder: ((context, state) => const SettingsScreen()),
+              ),
+            ],
+          ),
+        ],
       ),
     ],
   );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:applylog/core/notifications/prsentations/providers/notification_provider.dart';
-import 'package:applylog/core/notifications/prsentations/providers/schedule_follow_up_provider.dart';
 import 'package:applylog/core/router/app_router.dart';
+import 'package:applylog/core/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -20,10 +20,14 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
+    final themeMode=ref.watch(themeProvider);
 
     return MaterialApp.router(
       routerConfig: router,
       debugShowCheckedModeBanner: false,
+      themeMode: themeMode,
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
     );
   }
 }
